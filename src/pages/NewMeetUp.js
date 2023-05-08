450e2b632f9edd35d657e45436263c2b2147f1f4
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2023-05-08 14:30:20 -0600

    working with forms

--- src/pages/NewMeetUp.js
+++ src/pages/NewMeetUp.js
@@ -1,5 +1,12 @@
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 function NewMeetUpPage() {
-  return <div>New Meet Up page</div>;
+  return (
+    <div>
+      <h1>New Meet Up page</h1>
+      <NewMeetupForm />
+    </div>
+  );
 }
 
 export default NewMeetUpPage;
