03e46449cf39a298bcdd8b9dca5c8e32d9ebe768
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2023-05-08 12:47:22 -0600

    update for new lesson

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import Todo from "./components/Todo";
-
 function App() {
   return (
     <div>
       <h1>My To Dos</h1>
-      <Todo text="Learn React" />
-      <Todo text="Master React" />
-      <Todo text="React Course" />
     </div>
   );
 }
