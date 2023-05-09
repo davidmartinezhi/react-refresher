3629b10971609e32dc4f4ead6e1d3705bdf25918
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2023-05-09 13:16:28 -0600

    sending post http request

--- src/pages/NewMeetUp.js
+++ src/pages/NewMeetUp.js
@@ -1,10 +1,23 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetUpPage() {
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-getting-started-a947c-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
+
   return (
     <div>
       <h1>New Meet Up page</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </div>
   );
 }
